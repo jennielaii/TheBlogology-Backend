c04c49650dd76ca66c509ae902f1d24a8e64cc34
jennielai <jennie.t.lai001@gmail.com>
2021-12-19 22:12:40 -0500

    added routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const routesReport = require('rowdy-logger').begin(app);
+
+app.use(require('morgan')('tiny'));
+app.use(express.json());
+app.use(require('cors')());
+
+const blogpostRoutes = require('./routes/blogpostRoute')
+app.use('/post', blogpostRoutes)
+
+const userRoutes = require('./routes/userRoute')
+app.use('/user', userRoutes)
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log('server listening on ${PORT}');
+    routesReport.print()
+});
